cc0b378be70a3cef15090f06ae5d7acceaf3143d
Ben_Linux <benoit.vincent2707@gmail.com>
2022-03-20 09:38:17 +0100

    Edit : replace adorable-avatars URL (down API) in FakeApiServiceGenerator to display Avatars

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.java
@@ -15,33 +15,33 @@ public abstract class FakeApiServiceGenerator {
     }
 
     public static List<User> FAKE_USERS = Arrays.asList(
-            new User("001", "Jake", "https://api.adorable.io/AVATARS/512/1.png"),
-            new User("002", "Paul", "https://api.adorable.io/AVATARS/512/2.png"),
-            new User("003", "Phil", "https://api.adorable.io/AVATARS/512/3.png"),
-            new User("004", "Guillaume", "https://api.adorable.io/AVATARS/512/4.png"),
-            new User("005", "Francis", "https://api.adorable.io/AVATARS/512/5.png"),
-            new User("006", "George", "https://api.adorable.io/AVATARS/512/6.png"),
-            new User("007", "Louis", "https://api.adorable.io/AVATARS/512/7.png"),
-            new User("008", "Mateo", "https://api.adorable.io/AVATARS/512/8.png"),
-            new User("009", "April", "https://api.adorable.io/AVATARS/512/9.png"),
-            new User("010", "Louise", "https://api.adorable.io/AVATARS/512/10.png"),
-            new User("011", "Elodie", "https://api.adorable.io/AVATARS/512/11.png"),
-            new User("012", "Helene", "https://api.adorable.io/AVATARS/512/12.png"),
-            new User("013", "Fanny", "https://api.adorable.io/AVATARS/512/13.png"),
-            new User("014", "Laura", "https://api.adorable.io/AVATARS/512/14.png"),
-            new User("015", "Gertrude", "https://api.adorable.io/AVATARS/512/15.png"),
-            new User("016", "Chloé", "https://api.adorable.io/AVATARS/512/16.png"),
-            new User("017", "April", "https://api.adorable.io/AVATARS/512/17.png"),
-            new User("018", "Marie", "https://api.adorable.io/AVATARS/512/18.png"),
-            new User("019", "Henri", "https://api.adorable.io/AVATARS/512/19.png"),
-            new User("020", "Rémi", "https://api.adorable.io/AVATARS/512/20.png")
+            new User("001", "Jake", "https://adorable-avatars.broken.services/1.png"),
+            new User("002", "Paul", "https://adorable-avatars.broken.services/2.png"),
+            new User("003", "Phil", "https://adorable-avatars.broken.services/3.png"),
+            new User("004", "Guillaume", "https://adorable-avatars.broken.services/4.png"),
+            new User("005", "Francis", "https://adorable-avatars.broken.services/5.png"),
+            new User("006", "George", "https://adorable-avatars.broken.services/6.png"),
+            new User("007", "Louis", "https://adorable-avatars.broken.services/7.png"),
+            new User("008", "Mateo", "https://adorable-avatars.broken.services/8.png"),
+            new User("009", "April", "https://adorable-avatars.broken.services/9.png"),
+            new User("010", "Louise", "https://adorable-avatars.broken.services/10.png"),
+            new User("011", "Elodie", "https://adorable-avatars.broken.services/11.png"),
+            new User("012", "Helene", "https://adorable-avatars.broken.services/12.png"),
+            new User("013", "Fanny", "https://adorable-avatars.broken.services/13.png"),
+            new User("014", "Laura", "https://adorable-avatars.broken.services/14.png"),
+            new User("015", "Gertrude", "https://adorable-avatars.broken.services/15.png"),
+            new User("016", "Chloé", "https://adorable-avatars.broken.services/16.png"),
+            new User("017", "April", "https://adorable-avatars.broken.services/17.png"),
+            new User("018", "Marie", "https://adorable-avatars.broken.services/18.png"),
+            new User("019", "Henri", "https://adorable-avatars.broken.services/19.png"),
+            new User("020", "Rémi", "https://adorable-avatars.broken.services/20.png")
     );
 
     public static List<User> FAKE_USERS_RANDOM = Arrays.asList(
-            new User("021", "Lea", "https://api.adorable.io/AVATARS/512/21.png"),
-            new User("022", "Geoffrey", "https://api.adorable.io/AVATARS/512/22.png"),
-            new User("023", "Simon", "https://api.adorable.io/AVATARS/512/23.png"),
-            new User("024", "André", "https://api.adorable.io/AVATARS/512/24.png"),
-            new User("025", "Leopold", "https://api.adorable.io/AVATARS/512/25.png")
+            new User("021", "Lea", "https://adorable-avatars.broken.services/21.png"),
+            new User("022", "Geoffrey", "https://adorable-avatars.broken.services/22.png"),
+            new User("023", "Simon", "https://adorable-avatars.broken.services/23.png"),
+            new User("024", "André", "https://adorable-avatars.broken.services/24.png"),
+            new User("025", "Leopold", "https://adorable-avatars.broken.services/25.png")
     );
 }
